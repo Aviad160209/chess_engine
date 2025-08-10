71b243edf3c101f89f6855f0ffccf64ee3b99de0
Aviad160209 <92807055+Aviad160209@users.noreply.github.com>
2025-08-10 06:08:42 +0300

    added Pawn and Knight Movement

--- stupid_chess.c
+++ stupid_chess.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*TO DO:
+add comments
+add knight movement
+add bishop, king and queen movement
+*/
 int main()
 {
     char chessBord[8][8];
@@ -8,7 +13,7 @@ int main()
     int col = 0;
     int amountOfMoves = 0;
 
-    // אתחול הלוח
+    // board setting
     for (row = 0; row < 8; row++)
     {
         for (col = 0; col < 8; col++)
@@ -71,19 +76,25 @@ int main()
     while (amountOfMoves == 0)
     {
         printf("white move: ");
-        fgets(move, sizeof(move), stdin);
+
+        if (fgets(move, sizeof(move), stdin) == NULL) {
+            printf("\nInput interrupted. Exiting...\n");
+            break;
+        }
 
         if (move[0] == 'r') 
         {
             int targetRow = move[2] - '1';
-            int targetCol=0;
+            int targetCol = 0;
 
             if (move[1] >= 'A' && move[1] <= 'H')
             {
                 targetCol = move[1] - 'A';
             }
             else if (move[1] >= 'a' && move[1] <= 'h')
+            {
                 targetCol = move[1] - 'a';
+            }
             else 
             {
                 printf("Invalid column\n");
@@ -98,8 +109,7 @@ int main()
                 {
                     if (chessBord[row][col] == 'r') 
                     {
-                        // באותה עמודה
-                        if (col == targetCol) 
+                        if (col == targetCol) // same column
                         {
                             int step = (targetRow > row) ? 1 : -1;
                             int clear = 1;
@@ -113,10 +123,12 @@ int main()
                             }
                             if (clear) 
                             {
-                                if (chessBord[targetRow][col] != '.' && chessBord[targetRow][col] >= 'b' && chessBord[targetRow][col] <= 'p') 
-                                {
-                                    break; // ניסיון לאכול את עצמך – לא נחשב מהלך חוקי
-                                }
+                                if (chessBord[targetRow][col] != '.' &&
+                                    chessBord[targetRow][col] >= 'b' &&
+                                    chessBord[targetRow][col] <= 'p')
+                                    {
+                                        break; 
+                                    }
                                 chessBord[targetRow][col] = 'r';
                                 chessBord[row][col] = '.';
                                 found = 1;
@@ -124,8 +136,7 @@ int main()
                                 break;
                             }
                         }
-                        // באותה שורה
-                        else if (row == targetRow) 
+                        else if (row == targetRow) // same line
                         {
                             int step = (targetCol > col) ? 1 : -1;
                             int clear = 1;
@@ -139,10 +150,13 @@ int main()
                             }
                             if (clear) 
                             {
-                                if (chessBord[row][targetCol] != '.' && chessBord[row][targetCol] >= 'b' && chessBord[row][targetCol] <= 'p') 
-                                {
-                                    break; // ניסיון לאכול את עצמך – לא נחשב מהלך חוקי
-                                }
+                                if (chessBord[row][targetCol] != '.' &&
+                                    chessBord[row][targetCol] >= 'b' &&
+                                    chessBord[row][targetCol] <= 'p')
+                                    {
+                                        break;
+                                    }
+
                                 chessBord[row][targetCol] = 'r';
                                 chessBord[row][col] = '.';
                                 found = 1;
@@ -154,13 +168,76 @@ int main()
                 }
             }
 
-            if (!found) 
+            if (!found)
             {
                 printf("Invalid move!\n");
             }
         }
 
-        // הדפסת מצב הלוח לאחר ניסיון תנועה
+        if (move[0] == 'p')
+        {
+            int targetRow = move[2] - '1';
+            int targetCol = 0;
+            int currntRow = 0;
+
+            if (move[1] >= 'A' && move[1] <= 'H')
+            {
+                targetCol = move[1] - 'A';
+            }
+            else if (move[1] >= 'a' && move[1] <= 'h')
+            {
+                targetCol = move[1] - 'a';
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                if (chessBord[i][targetCol] == 'p')
+                {
+                    currntRow = i;
+                    break;
+                }
+            }
+
+            if (targetRow <= currntRow + 2 && targetRow > currntRow)
+            {
+                chessBord[targetRow][targetCol] = 'p';
+                chessBord[currntRow][targetCol] = '.';
+                amountOfMoves++;
+            }
+            else
+            {
+                printf("illegal move\n");
+            }
+        }
+        if (move[0] == 'n')
+        {
+            int targetRow = move[2] - '1';
+            int targetCol = 0;
+            int currntRow = 0;
+        
+            if (move[1] >= 'A' && move[1] <= 'H')
+            {
+                targetCol = move[1] - 'A';
+            }
+            else if (move[1] >= 'a' && move[1] <= 'h')
+            {
+                targetCol = move[1] - 'a';
+            }
+            if (chessBord[targetRow-2][targetCol-1] == 'n')
+            {
+                chessBord[targetRow-2][targetCol-1] = '.';
+                chessBord[targetRow][targetCol] = 'n';
+                amountOfMoves++;
+            }
+            else if (chessBord[targetRow-2][targetCol+1] == 'n')
+            {
+                chessBord[targetRow-2][targetCol+1] = '.';
+                chessBord[targetRow][targetCol] = 'n';
+                amountOfMoves++;
+            }
+            /**/
+            
+        }
+        // printing of the board
         for (row = 0; row < 8; row++)
         {
             for (col = 0; col < 8; col++)
